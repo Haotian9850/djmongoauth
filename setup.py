a8bdb8bb0ff4a1e420e356613970624018911b68
Haotian9850 <haotian9850@gmail.com>
2021-10-20 17:39:43 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
+import pathlib
 from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name="djmongoauth",
     version="0.0.1",
-    description="Out-of-the-box support for user register, signin, email verification and password recovery workflow for websites built with django and mongo",
+    description="Out-of-the-box support register, sign in, email verification and password recovery workflows for websites based on Django and MongoDB",
     url="https://github.com/Haotian9850/djmongoauth",
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="hao",
     author_email="hl7gr@virginia.edu",
     license="MIT",
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+
+    ],
     packages=find_packages(exclude=["test", "djmongouser_legacy"]),
     zip_safe=False,
     install_requires=[
